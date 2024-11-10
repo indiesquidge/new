f809fdc7aeeb836dc8e332d9a52e5ffc71191031
Austin Wood <3409645+indiesquidge@users.noreply.github.com>
2024-11-10 14:03:48 -0600

    Add config for SPA app on GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,11 @@ declare module "@remix-run/node" {
 }
 
 export default defineConfig({
+  base: "/new/",
   plugins: [
     remix({
+      basename: "/new/",
+      ssr: false,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
